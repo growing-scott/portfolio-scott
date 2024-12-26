895b7a4458feaea0b0c4dbee8e53607307f2b411
Scott <coolrainy24@gmail.com>
2024-12-26 11:12:33 +0900

    configuration coupon api

---
+++ portfolio-coupon-api/src/main/java/com/example/portfoliocouponapi/PortfolioCouponApiApplication.java
@@ -0,0 +1,19 @@
+package com.example.portfoliocouponapi;
+
+import com.example.domainmysql.DomainMySqlConfiguration;
+import com.example.domainredis.DomainRedisConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+@Import({DomainMySqlConfiguration.class, DomainRedisConfiguration.class})
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class PortfolioCouponApiApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("spring.config.name", "application-coupon-api");
+        SpringApplication.run(PortfolioCouponApiApplication.class, args);
+    }
+
+}
---
+++ portfolio-coupon-api/src/test/java/com/example/portfoliocouponapi/PortfolioCouponApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfoliocouponapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioCouponApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
